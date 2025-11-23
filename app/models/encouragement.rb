4186b965107265e470f0e996c2835a4c19c98009
Andi Messingschlager <andi.messings@gmail.com>
2025-11-23 20:23:37 +0100

    Encouragements

--- app/models/encouragement.rb
+++ app/models/encouragement.rb
@@ -3,4 +3,7 @@ class Encouragement < ApplicationRecord
   belongs_to :receiver, class_name: "User"
 
   validates :body, presence: true, length: { maximum: 500 }
+
+  after_create_commit -> { broadcast_prepend_later_to [receiver, :encouragements] }
+  after_update_commit -> { broadcast_replace_later_to [receiver, :encouragements] }
 end
