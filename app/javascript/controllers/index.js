41251cdc1f1c3756e64257c62d9e0e4b0e9c1c93
Cyrielle THOUMY <cyriellethoumy@Mac.lan>
2025-11-23 20:55:44 +0000

    Fix Stimulus double registration

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-
-import FeatureDropdownController from "./feature_dropdown_controller"
-application.register("feature-dropdown", FeatureDropdownController)
-
